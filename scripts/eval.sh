61564362d1f4c0326181d0c38f40f1cc99d50a7e
Stephen Peng <stephen@ip-172-31-7-89.us-west-2.compute.internal>
2023-03-01 03:53:11 +0000

    added custom scripts

---
+++ scripts/eval.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+output="./output/output.tsv"
+file="./stanza/TOC_Utility/Processed_Data/synth2.test.json"
+while getopts o:i:m: flag
+do
+    case "${flag}" in
+        o) output=${OPTARG};;
+        i) file=${OPTARG};;
+        m) model=${OPTARG};;
+    esac
+done
+
+[ -z "$model" ] && echo "Please enter a model with the [-m] flag!" && exit 2;
+
+echo "Evaluating $model on $file... Output will be written to $output"
+
+python3 stanza/models/data_extractor.py \
+	--data_dir ./data \
+	--eval_file  "$file" \
+	--eval_output_file  "$output" \
+	--wordvec_pretrain_file /home/stephen/stanza_resources/en/pretrain/combined.pt \
+	--charlm \
+	--charlm_shorthand 1billion \
+	--charlm_forward_file /home/stephen/stanza_resources/en/forward_charlm/1billion.pt \
+	--charlm_backward_file /home/stephen/stanza_resources/en/backward_charlm/1billion.pt \
+	--mode predict \
+	--save_dir ./models/data_extractor/"$model" \
+	--shorthand en_"$model"
