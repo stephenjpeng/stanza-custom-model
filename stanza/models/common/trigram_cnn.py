f450b3575326def72ed0cc568db866039634a6c2
Stephen Peng <stephen.j.peng@gmail.com>
2023-03-07 16:33:45 -0800

    updated trigram_cnn switch

--- stanza/models/common/trigram_cnn.py
+++ stanza/models/common/trigram_cnn.py
@@ -7,14 +7,16 @@ from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence, pack_s
 class TrigramCNN(nn.Module):
     def __init__(self, input_size, hidden_size):
         super().__init__()
-        self.mlp1 = nn.Linear(input_size * 3, hidden_size)
-        self.mlp2 = nn.Linear(hidden_size, input_size)
-        self.relu = nn.ReLU()
+        self.mlp = nn.Sequential(
+            nn.Linear(input_size * 3, hidden_size),
+            nn.GELU(),
+            nn.Linear(hidden_size, input_size),
+            nn.GELU(),
+        )
 
 
     def forward(self, input):
         x = F.pad(input, (0, 0, 1, 1), 'constant', 0)
         x = x.unfold(1, 3, 1).mT.flatten(2, 3)
-        x = self.relu(self.mlp1(x))
-        x = self.mlp2(x)
+        x = self.mlp(x)
         return input + x
