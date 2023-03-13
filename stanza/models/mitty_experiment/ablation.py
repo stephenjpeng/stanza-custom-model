d863dc9dfb73fffb32ab8e377e4584994f00a598
Mitty_Yu <mittyu@stanford.edu>
2023-03-13 13:29:56 -0700

    ablation

---
+++ stanza/models/mitty_experiment/ablation.py
@@ -0,0 +1,11 @@
+/#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   ablation.py
+@Time    :   2023/03/11 15:31:54
+@Author  :   MittyYu 
+@Desc    :   None
+'''
+
+from pykeen.ablation import ablation_pipeline
+
